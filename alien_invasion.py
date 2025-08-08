5937154dd364964971025f2975fc3afb4c4ffb6d
Aditya Pratap Singh <adiuk52@iitkgp.ac.in>
2025-08-09 02:51:22 +0530

    Added bi-directional movement to ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,13 +19,23 @@ class AlienInvasion :
 
 	def _check_events(self):
 		for event in pygame.event.get():
-			print(event)
+
 			if event.type == pygame.QUIT:
 				sys.exit()
 			
-			if event.type == pygame.TEXTINPUT:
-				if event.text == 'l':
-					self.ship.rect.x += 1
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RIGHT:
+					self.ship.move_right = True
+				if event.key == pygame.K_LEFT:
+					self.ship.move_left = True
+			
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_RIGHT:
+					self.ship.move_right = False
+
+				if event.key == pygame.K_LEFT:
+					self.ship.move_left = False
+
 
 	def _update_screen(self):
 		self.screen.fill(self.settings.bg_color)
@@ -38,6 +48,7 @@ class AlienInvasion :
 			
 			self._check_events()			
 			self._update_screen()
+			self.ship.update()
 			pygame.display.flip()
 			self.clock.tick(60)
 
